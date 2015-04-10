245e9a3f9256dbcd2790208dd6d6280d19bc7fde
Giovanni Ponciano <ponciano.giovanni15@gmail.com>
2015-04-10 11:29:04 -0700

    puhsing

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -55,7 +55,7 @@ game.PlayerEntity = me.Entity.extend({
     
     update: function(delta){
         this.now= new Date().getTime();  
-        this.dead = checkIfDead();
+        this.dead = this.checkIfDead();
         this.checkKeyPressesAndMove();        
         this.setAnimation();          
         me.collision.check(this, true, this.collideHandler.bind(this), true);
@@ -138,6 +138,13 @@ game.PlayerEntity = me.Entity.extend({
     
     collideHandler: function(response){
         if(response.b.type==='EnemyBaseEntity'){
+            this.collideWithEnemyBase(response);
+        }else if(response.b.type==='EnemyCreep'){
+            this.collideWithEnemyCreep(response);
+        }
+    },
+    
+    collideWithEnemyBase: function(response){
             var ydif = this.pos.y - response.b.pos.y;
             var xdif = this.pos.x - response.b.pos.x;
             
@@ -147,47 +154,58 @@ game.PlayerEntity = me.Entity.extend({
             }
             else if(xdif>-35 && this.facing==='right' && (xdif<0)){
                 this.body.vel.x = 0;
-               // this.pos.x = this.pos.x -1;
             }else if(xdif<70 && this.facing==='left' && xdif>0){
                 this.body.vel.x = 0;
-                this.pos.x = this.pos.x +1;
-            }
-            
-            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer){
-                
+            }      
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer){               
                 this.lastHit = this.now;
                 response.b.loseHealth(game.data.playerAttack);
             }
-        }else if(response.b.type==='EnemyCreep'){
+    },
+    
+    collideWithEnemyCreep: function(response){   
             var xdif = this.pos.x - response.b.pos.x;
             var ydif = this.pos.y - response.b.pos.y;
             
-            if (xdif>0){
-                //this.pos.x = this.pos.x + 1;
+            this.stopMovement(xdif);     
+            
+            if(this.checkAttack(xdif, ydif)){
+               this.hitCreep(response);             
+            };
+    },
+    
+    stopMovement: function(xdif){
+        if (xdif>0){
                 if(this.facing==="left"){
                     this.body.vel.x = 0;
                 }
             }else{
-                //this.pos.x = this.pos.x - 1;
                 if(this.facing==="right"){
                     this.body.vel.x = 0;
                 }
-            }           
-            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
+            }      
+    },
+    
+    checkAttack: function(xdif, ydif){
+        if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
                     && (Math.abs(ydif) <=40) && 
                     (((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
                     ){  
                 this.lastHit = this.now;
-                //if the creeps health is less than our attack, execute code in if statement              
-                if(response.b.health <= game.data.playerAttack){
+                //if the creeps health is less than our attack, execute code in if statement  
+                return true;                 
+        }
+        return false;
+    },
+    
+    hitCreep: function(response){
+         if(response.b.health <= game.data.playerAttack){
                     //adds one gold for a creep kill
                     game.data.gold += 1;
                     console.log("Current gold: " + game.data.gold);
                 }
                 
-                response.b.loseHealth(game.data.playerAttack);   
-            }
-        }
+                response.b.loseHealth(game.data.playerAttack);
     }
 });
     
